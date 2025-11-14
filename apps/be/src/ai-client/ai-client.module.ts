439db49a164595700d64aa9798c6335439790327
garden <garden5479@gmail.com>
2025-11-14 19:54:17 +0900

    mod: ai 서버 설정

--- apps/be/src/ai-client/ai-client.module.ts
+++ apps/be/src/ai-client/ai-client.module.ts
@@ -2,9 +2,18 @@ import { Module } from "@nestjs/common";
 import { AiClientController } from "./ai-client.controller";
 import { AiClientService } from "./ai-client.service";
 import { HttpModule } from "@nestjs/axios";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        baseURL: configService.get("AI_BASE_URL"),
+        timeout: 5000,
+      }),
+    }),
+  ],
   exports: [AiClientService],
   controllers: [AiClientController],
   providers: [AiClientService],
