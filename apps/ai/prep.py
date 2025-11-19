63c4c8493ca5b7505dc86d04dbf5c06651531cad
donngas <geunhyeongsam@gmail.com>
2025-11-19 16:50:01 +0900

    chore: implement loguru for prep and prepare for overhaul

--- apps/ai/prep.py
+++ apps/ai/prep.py
@@ -5,20 +5,23 @@ import os
 import json
 import time
 from sentence_transformers import SentenceTransformer
+from typing import List, Dict, Any
+from PIL import Image
+from loguru import logger
 
 load_dotenv()
 
 client = genai.Client()
 
 # Extract OCR, caption, and explain why this meme is funny. Return as JSON with keys {ocr, caption, humor}.
-prompt = """
+PROMPT = """
 밈 이미지에 대해서 [OCR, 캡션, 이 밈이 재미있는 이유 설명]을 추출해. {ocr, caption, humor} 형식의 JSON으로 반환해.
 """
+EMBEDDING_MODEL = "snunlp/KR-SBERT-V40K-klueNLI-augSTS"
 
 # Configs (Paths are for seeding purpose run)
-metadata_output = "./seed/tmp/images.json"
-embedding_model = "snunlp/KR-SBERT-V40K-klueNLI-augSTS"
-embedding_output = "./seed/tmp/embeddings.json"
+METADATA_OUTPUT = "./seed/tmp/images.json"
+EMBEDDING_OUTPUT = "./seed/tmp/embeddings.json"
 
 # Define response format for Gemini Structured Response
 class MemeMeta(BaseModel):
@@ -41,7 +44,7 @@ def process_image(img_path):
                 data=image_bytes,
                 mime_type='image/jpeg'
             ),
-            prompt
+            PROMPT
         ],
         # Structured Response config
         config={
@@ -52,37 +55,95 @@ def process_image(img_path):
 
     return response.text
 
+def load_image_size(path: str):
+    try:
+        with Image.open(path) as img:
+            return img.width, img.height
+    except:
+        return 0, 0
+
 # Prep config, subject to change
 imgpath = ""
 batch_size = 100
 max_total = None
 sleep_sec = 0
 
-# File setup, subject to change to accomodate Google Cloud Storage
-files = [f for f in os.listdir(imgpath)]
-if max_total is not None:
-    files = files[:max_total]
+# Function to return the list of image files in given directory. Subject to change for Google Cloud Storage integration
+def load_files(imgpath: str, max_total: int = None) -> List[str]:
 
-# Loop for prep, subject to be made into a function for automation
-for i in range(0, len(files), batch_size):
+    # Check if path exists
+    if not os.path.isdir(imgpath):
+        logger.error(f"Directory not fount at {imgpath}")
+        return []
+    
+    # List files that end with right extensions
+    files = [f for f in os.listdir(imgpath) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.webp'))]
+    
+    # Check max_total
+    if max_total is not None:
+        files = files[:max_total]
+    
+    logger.info(f"Found {len(files)} total image files to process.")
+    return files
 
-    batch = files[i:i+batch_size]
-    print(f"Processing batch {i//batch_size+1}: {len(batch)} images.")
+# Function to process batches
+def process_batch(
+        imgpath: str,
+        batch_files: List[str],
+        batch_index: int,
+        prompt: str,
+        sleep_sec: float = 0
+) -> None:
+    logger.info(f"\nProcessing batch {batch_index}: {len(batch_files)} images.")
     results = {}
-
     batch_start = time.time()
 
-    for fname in batch:
+    for fname in batch_files:
         path = os.path.join(imgpath, fname)
         try:
-            result = process_image(path)
-            results[fname] = json.loads(result)
+            result = process_image(path, prompt)
+            results[fname] = result
         except Exception as e:
-            print(f"Error with {fname}: {e}")
-        time.sleep(sleep_sec)
+            logger.info(f"Error with image {fname}: {e}.")
 
+        if sleep_sec > 0:
+            time.sleep(sleep_sec)
+    
     batch_time = time.time() - batch_start
-    print(f"[Batch {batch_size}] completed in {batch_time:.1f}s")
+    logger.info(f"Batch {batch_index} completed in {batch_time:.1f}s.")
 
-    with open(f"metadata_batch_{i//batch_size + 1}.json", "w", encoding="utf-8") as f:
+    # Save results for the current batch.
+    # Currently for seeding purpose. To be integrated with Prisma Client.
+    output_fname = f"metadata_batch_{batch_index}.json"
+    with open(output_fname, "w", encoding="utf-8") as f:
         json.dump(results, f, ensure_ascii=False, indent=2)
+    logger.info(f"Results saved to {output_fname}")
+
+def build_embeddings(meta_dir: str) -> None:
+    pass
+
+def main(
+        imgpath: str,
+        batch_size: int = 100,
+        max_total: int = None,
+        sleep_sec: float = 0
+) -> None:
+    
+    files = load_files(imgpath, max_total)
+    if not files:
+        logger.error("No files to process. Exiting.")
+        return
+    
+    for i in range(0, len(files), batch_size):
+        batch = files[i:i+batch_size]
+        batch_index = i // batch_size + 1
+
+        process_batch(imgpath, batch, batch_index, PROMPT, sleep_sec)
+
+    logger.complete("Caption processing complete.")
+
+    if __name__ == "__main__":
+
+        image_dir = "./.seed_images"
+
+        main(image_dir, batch_size=30, max_total=None, sleep_sec=0.1)
