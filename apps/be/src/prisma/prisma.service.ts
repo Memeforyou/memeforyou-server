31feb2e32b89532c218f0f5e20d6b262af689690
garden <garden5479@gmail.com>
2025-11-23 12:23:53 +0900

    fix: try for deploy

--- apps/be/src/prisma/prisma.service.ts
+++ apps/be/src/prisma/prisma.service.ts
@@ -1,4 +1,22 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+// import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+// import { PrismaClient } from "@prisma/client";
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   // Nest application 시작될 때 DB 연결
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+//   // Nest application 종료될 때 DB 연결 끊기다
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
+
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
@@ -6,11 +24,10 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  // Nest application 시작될 때 DB 연결
   async onModuleInit() {
     await this.$connect();
   }
-  // Nest application 종료될 때 DB 연결 끊기
+
   async onModuleDestroy() {
     await this.$disconnect();
   }
