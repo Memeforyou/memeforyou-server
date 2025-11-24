38b13fa52bce1b937f1a3a5fad598a135900e526
donngas <geunhyeongsam@gmail.com>
2025-11-24 14:49:05 +0900

    chore: rearrange dir for better management

---
+++ apps/ai/utils/dbhandler.py
@@ -0,0 +1,27 @@
+from prisma import Prisma, register
+from prisma.models import tag, image, embedding
+from schema import ImageTrivial
+from typing import List
+
+db = Prisma()
+register(db)
+
+# --- async ---
+async def get_meta(inputs: List[int]) -> List[ImageTrivial]:
+
+    await db.connect()
+
+    rets = []
+
+    for id in inputs:
+
+        i_image = await db.image.find_unique(
+            where={
+                'image_id': id
+            }
+        )
+
+        t_image = ImageTrivial(image_id=i_image['image_id'], caption=i_image['caption'])
+        rets.append(t_image)
+
+    return rets
