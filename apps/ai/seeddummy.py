a2c3eda0be2490dc2b3545078a8f974d34ed13dc
donngas <geunhyeongsam@gmail.com>
2025-11-24 19:45:24 +0900

    fix: exclude embeddings from seeding script

--- apps/ai/seeddummy.py
+++ apps/ai/seeddummy.py
@@ -1,13 +1,12 @@
 import asyncio
 import json
 from prisma import Prisma, register
-from prisma.models import tag, image, embedding
+from prisma.models import tag, image
 from loguru import logger
 
 # Define seed file paths
 TAGS_PATH = "./seed/tags.json"
 IMAGES_PATH = "./seed/images.json"
-EBDS_PATH = "./seed/embeddings.json"
 
 # Util function to open and load json file
 def load_json(path: str):
@@ -34,11 +33,6 @@ async def main() -> None:
     logger.info("Beginning Images seeding...")
     images_count, images_total = await seed_images(db, IMAGES_PATH)
     logger.success(f"Images seeded successfully. {images_count} out of {images_total} samples.")
-
-    # Seed Embeddings
-    logger.info("Beginning Embeddings seeding...")
-    ebds_count, ebds_total = await seed_embeddings(db, EBDS_PATH)
-    logger.success(f"Embeddings seeded successfully. {ebds_count} out of {ebds_total} samples.")
 
     # Disconnect from DB
     await db.disconnect()
@@ -73,33 +67,6 @@ async def seed_images(db: Prisma, path):
         await image.prisma(db).create(data=row)
         logger.trace(f"Seeded Image {idx}/{total}")
         count += 1
-
-    return count, total
-
-# Function to seed Embeddings
-async def seed_embeddings(db: Prisma, path):
-
-    # Load embeddings.json from path
-    data = load_json(path)
-    total = len(data)
-    count = 0
-
-    # Insert operations
-    for idx, row in enumerate(data, start=1):
-
-        # Load data
-        image_id = row['image_id']
-        vector_data = '(' + ','.join(map(str, row['vector'])) + ')'
-
-        # Raw SQL query
-        sql = 'INSERT INTO embedding (image_id, vector) VALUES (?, ?)'
-        rows_affected = await db.execute_raw(sql, image_id, vector_data)
-
-        if rows_affected > 0:
-            logger.trace(f"Seeded Embedding {idx}/{total} for image_id: {image_id}")
-            count += 1
-        else:
-            logger.warning(f"Failed to seed Embedding {idx}/{total} for image_id: {image_id}")
 
     return count, total
 
