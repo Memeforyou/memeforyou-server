87dd7e5cabae32b8bc5ee245882616312ffd907b
donngas <geunhyeongsam@gmail.com>
2025-11-29 22:46:20 +0900

    feat: implement dedicated converter between captioning and updating captioned

--- apps/ai/preps/captioner.py
+++ apps/ai/preps/captioner.py
@@ -6,8 +6,8 @@ from .dblite import get_memes, update_captioned
 from itertools import batched
 from loguru import logger
 from dotenv import load_dotenv
-from os import getenv
-from typing import List, Dict, Any
+from os import getenv, path
+from typing import List, Tuple, Dict, Any
 
 load_dotenv()
 
@@ -19,8 +19,9 @@ SYSPROMPT = """
 제시된 json 스키마에 따라서 반환해.
 """
 AVAIL_TAGS = ["웃긴", "슬픈", "귀여운", "동물", "사람", "카툰", "캐릭터"]
+BPATH = "./downloads/"
 
-def gemini_caption(sys_prompt: str, available_tags: List[str], img_path: str) -> IndvCaption:
+def gemini_caption(sys_prompt: str, available_tags: List[str], img_id: int, img_path: str) -> IndvCaption:
     """
     Makes a structured content generation call to the Gemini API.
     """
@@ -41,6 +42,7 @@ def gemini_caption(sys_prompt: str, available_tags: List[str], img_path: str) ->
             model="gemini-2.5-flash",
             contents=[
                 f"이용 가능한 태그 목록: {', '.join(available_tags)}",
+                f"이번 이미지의 image_id: {img_id}",
                 genai.types.Part.from_bytes(
                 data=image_bytes,
                 mime_type='image/jpeg'
@@ -76,25 +78,70 @@ def fetch_and_batch(n: int = 100) -> List[List[Row]]:
 
     return batches
 
-def caption_rows(target_rows: list) -> List[IndvCaption]:
+def caption_rows(target_rows: list, base_path: str) -> List[IndvCaption]:
     """
     Generates captions for input rows of memes,
     and returns a list of IndvCaption containing the image_id, caption, and tags.
     """
 
-    captioned_rows = []
+    # Initialize update list
+    local_captioned_rows = []
+
+    # Identify batch range for logging purposes
+    first_meme_id = target_rows[0]['image_id']
+    last_meme_id = target_rows[-1]['image_id']
+    logger.debug(f"Captioning this batch: {first_meme_id} to {last_meme_id}...")
 
     # Perform gemini_caption on provided batch
-    pass
+    for i, row in enumerate(target_rows):
 
-    return captioned_rows
+        # Acquire filename based on iteration
+        image_id = row['image_id']
+        fname = str(image_id)+'.jpg'
+        img_path = path.join(base_path, fname)
+
+        logger.trace(f"Acquired file path for {i}th image in this batch: ...{fname}. Now requesting...")
+
+        res = gemini_caption(sys_prompt=SYSPROMPT, available_tags=AVAIL_TAGS, img_id=image_id, img_path=img_path)
+        local_captioned_rows.append(res)
+        logger.trace(f"{i}th image in this batch captioned and appended to return.")
+
+    return local_captioned_rows
+
+def caption_converter(before_rows: List[IndvCaption]) -> Tuple[List[int], List[str], List[List[str]]]:
+
+    ids: List[int] = []
+    captions: List[str] = []
+    tags: List[List[str]] = []
+
+    for row in before_rows:
+        ids.append(row.image_id)
+        captions.append(row.caption)
+        tags.append(row.tags)
+
+    return ids, captions, tags
+
 
 def captioner_operation() -> None:
 
-    captioned_rows = caption_rows()
+    # Initialize update list
+    global_captioned_rows = []
 
-    if captioned_rows:
-        update_captioned()
+    # Acquire batches
+    batches = fetch_and_batch()
+
+    # Iterate through batches
+    for i, batch in enumerate(batches):
+        
+        # Caption each batch
+        logger.debug(f"Beginning captioning for batch {i}...")
+        this_batch_captions = caption_rows(target_rows=batch, base_path=BPATH)
+        global_captioned_rows.extend(this_batch_captions)
+
+    complete_id, compete_captions, complete_tags = caption_converter(before_rows=global_captioned_rows)
+
+    if global_captioned_rows:
+        update_captioned(image_ids=complete_id, captions=compete_captions, tags_list=complete_tags)
     else:
         logger.warning("No captioned rows returned by caption_rows.")
 
