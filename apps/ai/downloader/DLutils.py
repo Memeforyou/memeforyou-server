84feaa0f57d34b68250b1c393466acf5356e1c0e
donngas <geunhyeongsam@gmail.com>
2025-11-30 03:04:07 +0900

    feat: define schemas used during downloading

--- apps/ai/downloader/DLutils.py
+++ apps/ai/downloader/DLutils.py
@@ -1,4 +1,20 @@
 from PIL import Image
+from pydantic import BaseModel
+
+class IndvImageURL(BaseModel):
+    original_url: str
+    src_url: str
+
+class ImageDL(BaseModel):
+    success: bool
+    width: int
+    height: int
+
+def download_image(url: str, save_path: str):
+    """
+    Download image to given path by url.
+    """
+    pass
 
 def convert_to_jpg():
     """
