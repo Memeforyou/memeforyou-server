dbb23ec37d5980e2b49cd391f5e8675263dd06b4
donngas <geunhyeongsam@gmail.com>
2025-11-30 01:03:22 +0900

    feat: create utility file for downloader

---
+++ apps/ai/downloader/DLutils.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+def convert_to_jpg():
+    """
+    Convert image format to jpg if it has different extension.
+    """
+    pass
+
+def minsize_filter():
+    """
+    Filter image by minimum size
+    """
+    pass
