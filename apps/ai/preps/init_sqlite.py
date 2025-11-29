a8bbf01370fc80cee900360c09d3be63715a5d8a
donngas <geunhyeongsam@gmail.com>
2025-11-29 13:12:22 +0900

    fix: initiate tag table as well

--- apps/ai/preps/init_sqlite.py
+++ apps/ai/preps/init_sqlite.py
@@ -26,4 +26,15 @@ CREATE TABLE IF NOT EXISTS ImageTag (
     FOREIGN KEY (image_id) REFERENCES Image(image_id)
 )
 """)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Tag (
+    tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tag_name TEXT NOT NULL UNIQUE
+)
+""")
+tags_to_insert = ["웃긴", "슬픈", "귀여운", "동물", "사람", "카툰", "캐릭터"]
+cursor.executemany(
+    "INSERT OR IGNORE INTO Tag (tag_name) VALUES (?)",
+    tags_to_insert
+)
 conn.commit()
